dce3a80d1c70cbebdaa77d68432e6c9c0f91ad6e
Unchun Yang <ucyang@ucyang.com>
2021-08-06 23:59:01 +0900

    브라우저 인식 및 파이어폭스에서 새 탭이 열릴 때 기존 탭의 뒤로가기 문제 해결

--- NaverCafeFreePass/background.js
+++ NaverCafeFreePass/background.js
@@ -3,6 +3,13 @@
 if (typeof browser === "undefined")
   var browser = chrome;
 
+var isFirefox = false;
+
+if (typeof browser.runtime.getBrowserInfo === "function")
+  browser.runtime.getBrowserInfo(function(info) {
+    isFirefox = /firefox/i.test(info.name);
+  });
+
 function getLocation(href) {
   var l = document.createElement("a");
   l.href = href;
@@ -62,7 +69,7 @@ function createTab(details) {
         browser.tabs.create({url: getShortCafeAddress(cafeName, articleid, staffOnly),
           index: tab.index + 1},
           function(tab) {
-            browser.tabs.executeScript(originalTabId, {code: "window.history.back()"});
+            browser.tabs.executeScript(originalTabId, {code: isFirefox ? "" : "window.history.back()"});
           });
       } else {
         alert("Naver Cafe Free Pass 확장 프로그램이 게시글 주소를 인식하지 못했습니다. "
@@ -71,7 +78,7 @@ function createTab(details) {
           + "게시글 링크를 우클릭 후 새 탭에서 열어보시거나 "
           + "일시적으로 확장 프로그램을 끄고 접속해주시기 바랍니다. "
           + "불편이 계속되실 경우 개발자에게 문의를 남겨주시면 성실히 답변드리겠습니다.");
-        browser.tabs.executeScript(originalTabId, {code: "window.history.back()"},
+        browser.tabs.executeScript(originalTabId, {code: isFirefox ? "" : "window.history.back()"},
           function(result) {
             browser.runtime.lastError;
           });
@@ -168,7 +175,7 @@ function onBeforeRequestListenerDaum(details) {
             browser.tabs.create({url: "https://cafe.daum.net/" + cafeName
               + "/" + fldid + "/" + datanum + "?q=t", index: tab.index + 1},
               function(tab) {
-                browser.tabs.executeScript(originalTabId, {code: "window.history.back()"});
+                browser.tabs.executeScript(originalTabId, {code: isFirefox ? "" : "window.history.back()"});
               });
           else
             browser.tabs.remove(tab.id, function() {
